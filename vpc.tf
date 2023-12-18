7ab6610a76af2cc9816a5948f1b2cc7ed72dbea3
Pliffisson <pliffissongomes@gmail.com>
2023-12-18 12:00:16 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "PublicSubnetWeb1" {
 
 resource "aws_subnet" "PublicSubnetWeb2" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
 
   tags = {
     Name = "MyPublicWeb2"
